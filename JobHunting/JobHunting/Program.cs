66839e38ded56c72b4c582ae545127804637c25a
Tony <godjames226@gmail.com>
2024-08-26 00:07:52 +0800

    套板至layout

--- JobHunting/JobHunting/Program.cs
+++ JobHunting/JobHunting/Program.cs
@@ -6,10 +6,10 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<DuckContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Duck"));
-});
+//builder.Services.AddDbContext<DuckContext>(options =>
+//{
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("Duck"));
+//});
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
