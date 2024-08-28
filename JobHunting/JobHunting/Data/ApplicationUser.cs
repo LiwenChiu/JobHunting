9efd050a425cd3fc62dfa1f2fe9a707e061a946b
Chiu <liwen2391062@gmail.com>
2024-08-28 17:10:04 +0800

    add letters card add login/register

---
+++ JobHunting/JobHunting/Data/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace JobHunting.Data
+{
+    public class ApplicationUser:IdentityUser
+    {
+        [MaxLength(10)]
+        public string NationalID { get; set; }
+    }
+    
+}
---
+++ JobHunting/JobHunting/Data/Migrations/20240827173317_Add ID.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobHunting.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddID : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NationalID",
+                table: "AspNetUsers",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NationalID",
+                table: "AspNetUsers");
+        }
+    }
+}
