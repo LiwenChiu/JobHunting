ed9e5833418d14b4a814cd7da989148a021d21aa
Chiu <liwen2391062@gmail.com>
2024-09-04 11:35:26 +0800

    companies partialView change to view

---
+++ JobHunting/JobHunting/Areas/Companies/Controllers/C_ResumeController.cs
@@ -0,0 +1,47 @@
+﻿using JobHunting.Areas.Companies.Models;
+using JobHunting.Areas.Companies.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobHunting.Areas.Companies.Controllers
+{
+    [Area("Companies")]
+    public class C_ResumeController : Controller
+    {
+        private readonly DuckCompaniesContext _context;
+
+        public C_ResumeController(DuckCompaniesContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ResumeStorage()
+        {
+            return View();
+        }
+        public IActionResult ReceiveResume()
+        {
+            var Openings = _context.Openings;
+            var TitleClasses = _context.TitleClasses;
+            var ResumeOpeningRecords = _context.ResumeOpeningRecords;
+            ViewBag.TitleClass = new SelectList(TitleClasses, "TitleClassID", "TitleClassName");
+            return View(Openings.Select(p => new ReceiveResumeViewModel
+            {
+                Title = p.Title,
+                TitleClassName = TitleClasses.Where(s => s.TitleClassID == p.TitleClassID).Select(s => s.TitleClassName).Single(),
+                ApplyDate = ResumeOpeningRecords.Where(a => a.OpeningID == p.OpeningID).Select(a => a.ApplyDate).Single(),
+            }));
+        }
+        public IActionResult Opening(string id)
+        {
+            var Openings = _context.Openings;
+            ViewBag.Opening = new SelectList(Openings.Where(o => o.TitleClassID == id), "TitleClassID", "TitleClassName");
+            return PartialView("_Opening");
+        }
+    }
+    
+}
