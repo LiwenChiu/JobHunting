55df0e388660e072329dded62d742eccf1be0d73
Tony <godjames226@gmail.com>
2024-09-08 02:22:25 +0800

    update companyIndex 能力標籤下拉選單

---
+++ JobHunting/JobHunting/ViewModel/TagSelectList.cs
@@ -0,0 +1,9 @@
+﻿namespace JobHunting.ViewModel
+{
+    public class TagSelectList
+    {
+        public int? TagClassId { get; set; }
+        public string TagClassName { get; set; }
+        public object TagObj { get; set; }
+    }
+}
