f529cd878617ac9d0ade030ac24d7ef48709100e
Tony <godjames226@gmail.com>
2024-09-09 18:27:15 +0800

    add _layout意見信件發送

---
+++ JobHunting/JobHunting/ViewModel/InsterLetter.cs
@@ -0,0 +1,11 @@
+﻿namespace JobHunting.ViewModel
+{
+    public class InsterLetter
+    {
+        public int? CompanyId { get; set; }
+        public int? CandidateId { get; set; }
+        public string Letterclass { get; set; }
+        public string SubjectLine { get; set; }
+        public string Content { get; set; }
+    }
+}
