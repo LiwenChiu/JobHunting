34294d5a935e6497ea416895c736b864c8382ead
Tony <godjames226@gmail.com>
2024-09-10 21:04:19 +0800

    add companies_公司介紹

---
+++ JobHunting/JobHunting/Areas/Companies/Controllers/CompanyIntroController.cs
@@ -0,0 +1,48 @@
+﻿using JobHunting.Areas.Companies.Models;
+using JobHunting.Areas.Companies.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace JobHunting.Areas.Companies.Controllers
+{
+    [Area("Companies")]
+    public class CompanyIntroController : Controller
+    {
+        private readonly DuckCompaniesContext _context;
+
+        public CompanyIntroController(DuckCompaniesContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public async Task<JsonResult> CompanyIntroList(int id)
+        {
+            return Json(_context.Companies.Include(a => a.CompanyClass).Where(c => c.CompanyId == id).Select(b => new
+            {
+                CompanyId = b.CompanyId,
+                CompanyName = b.CompanyName,
+                CompanyClassId = b.CompanyClassId,
+                Address = b.Address,
+                Intro = b.Intro,
+                Benefits = b.Benefits,
+                //Picture = null,
+                ContactName = b.ContactName,
+                ContactPhone = b.ContactPhone,
+                ContactEmail = b.ContactEmail,
+                CompanyClassName = b.CompanyClass.CompanyClassName
+            }));
+        }
+        public async Task<FileResult> GetPicture(int id)
+        {
+            //string FileName = Path.Combine( "images", "No_Image_Available.jpg");
+            Company c = await _context.Companies.FindAsync(id);
+            byte[] ImageContent = c.Picture;
+            return File(ImageContent, "image/jpeg");
+        }
+    }
+}
