74622616175a3fa35ad143d09618dfc063917ccc
Daniel <dancraft3746@gmail.com>
2024-09-11 15:45:18 +0800

    推推

---
+++ JobHunting/JobHunting/Areas/Candidates/ViewModels/GetCandidateMemberDataViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace JobHunting.Areas.Candidates.ViewModels
+{
+    public class GetCandidateMemberDataViewModel
+    {
+        public string Name { get; set; }
+
+        public byte[] Headshot { get; set; }
+
+        public string? TitleClassName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+
+        public string EmploymentStatus { get; set; }
+    }
+}
