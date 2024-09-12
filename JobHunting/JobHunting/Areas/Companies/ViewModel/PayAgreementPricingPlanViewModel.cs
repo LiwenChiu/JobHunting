0db04793598169512cd1b0c3ce7fd2be15bd9a2a
Daniel <dancraft3746@gmail.com>
2024-09-12 13:56:06 +0800

    Companies方案選擇部分完成，欠:綁帳號、綁金流

--- JobHunting/JobHunting/Areas/Companies/ViewModel/PayAgreementPricingPlanViewModel.cs
+++ JobHunting/JobHunting/Areas/Companies/ViewModel/PayAgreementPricingPlanViewModel.cs
@@ -1,17 +1,15 @@
 ﻿namespace JobHunting.Areas.Companies.ViewModel
 {
-    public class PricingPlanOutputViewModel
+    public class PayAgreementPricingPlanViewModel
     {
         public int PlanId { get; set; }
 
         public string Title { get; set; }
 
-        public string Intro { get; set; }
-
-        public int Duration { get; set; }
-
         public decimal? Price { get; set; }
 
         public decimal? Discount { get; set; }
+
+        public int Duration { get; set; }
     }
 }
