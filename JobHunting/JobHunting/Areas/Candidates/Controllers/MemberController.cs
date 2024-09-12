5a237248f926264c86ae5cd0561414e27350eb5a
Daniel <dancraft3746@gmail.com>
2024-09-12 10:05:51 +0800

    db

--- JobHunting/JobHunting/Areas/Candidates/Controllers/MemberController.cs
+++ JobHunting/JobHunting/Areas/Candidates/Controllers/MemberController.cs
@@ -1,6 +1,7 @@
 ﻿using JobHunting.Areas.Candidates.Models;
 using JobHunting.Areas.Candidates.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
 using System.Net;
 
@@ -22,18 +23,19 @@ namespace JobHunting.Areas.Candidates.Controllers
             return View();
         }
 
-        //POST: Candidates/Member/getWholeCandidateMemberData
+        //POST: Candidates/Member/GetWholeCandidateMemberData
         [HttpPost]
         //[ValidateAntiForgeryToken]
-        public async Task<IEnumerable<GetWholeCandidateMemberDataViewModel>> getWholeCandidateMemberData([FromBody]int id)
+        public async Task<IEnumerable<GetWholeCandidateMemberDataViewModel>> GetWholeCandidateMemberData([FromBody]int id)
         {
             return _context.Candidates
                 .Where(c => c.CandidateId == id)
                 .Select(c => new GetWholeCandidateMemberDataViewModel
                 {
+                    CandidateId = c.CandidateId,
                     Name = c.Name,
                     Email = c.Email,
-                    Sex = c.Sex == true ? "男" : "女",
+                    Sex = c.Sex,
                     Birthday = c.Birthday,
                     TitleClassId = c.TitleClassId,
                     Phone = c.Phone,
@@ -42,6 +44,51 @@ namespace JobHunting.Areas.Candidates.Controllers
                     EmploymentStatus = c.EmploymentStatus,
                     MilitaryService = c.MilitaryService
                 });
+        }
+
+        //POST: Candidates/Member/SaveEditMemberData
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public async Task<Array> SaveEditMemberData([FromBody][Bind("CandidateId,Name,Email,Sex,Birthday,TitleClassId,Phone,Address,Degree,EmploymentStatus,MilitaryService")] GetWholeCandidateMemberDataViewModel gwcmdvm)
+        {
+            string[] returnStatus = new string[2];
+
+            if (!ModelState.IsValid)
+            {
+                returnStatus = ["修改會員資料失敗", "失敗"];
+                return returnStatus;
+            }
+
+            var candidatememberData = await _context.Candidates.FindAsync(gwcmdvm.CandidateId);
+            if (candidatememberData == null)
+            {
+                returnStatus = ["會員資料不存在", "失敗"];
+                return returnStatus;
+            }
+            candidatememberData.Name = gwcmdvm.Name;
+            candidatememberData.Email = gwcmdvm.Email;
+            candidatememberData.Sex = gwcmdvm.Sex;
+            candidatememberData.Birthday = gwcmdvm.Birthday;
+            candidatememberData.TitleClassId = gwcmdvm.TitleClassId;
+            candidatememberData.Phone = gwcmdvm.Phone;
+            candidatememberData.Address = gwcmdvm.Address;
+            candidatememberData.Degree = gwcmdvm.Degree;
+            candidatememberData.EmploymentStatus = gwcmdvm.EmploymentStatus;
+            candidatememberData.MilitaryService = gwcmdvm.MilitaryService;
+
+            _context.Entry(candidatememberData).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                returnStatus = ["修改會員資料失敗", "失敗"];
+                return returnStatus;
+            }
+
+            returnStatus = [$"修改會員資料成功", "成功"];
+            return returnStatus;
         }
     }
 }
