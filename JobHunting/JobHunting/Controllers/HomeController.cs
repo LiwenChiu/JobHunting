ae03031b71357a9905f06c16af1d22b9aee6ab5d
Tony <godjames226@gmail.com>
2024-09-13 21:29:46 +0800

    update companyIntro 顯示畫面排版

--- JobHunting/JobHunting/Controllers/HomeController.cs
+++ JobHunting/JobHunting/Controllers/HomeController.cs
@@ -49,6 +49,7 @@ namespace JobHunting.Controllers
         [HttpPost]
         public async Task<IEnumerable<ResumesOutput>> SelectIndexList([FromBody] ResumeInputModel resume)
         {
+            EditResume(resume);
             var today = DateOnly.FromDateTime(DateTime.Now);
             var source = _context.Resumes.Include(a => a.Candidate).Include(x => x.Tags).ToList();
             if (resume.serchText.IsNullOrEmpty())
@@ -75,71 +76,72 @@ namespace JobHunting.Controllers
                     b.Address.Substring(4, 3) == resume.Area ||
                     (b.Address.Substring(4, 3) == resume.Area && b.Address.Substring(0, 3) == resume.zipCode) ||
                     //b.Degree.Contains(resume.Edu) ||
-                    b.skill.Any(z => z.TagId == resume.Skill))
-                                   .Select(x => new ResumesOutput
-                                   {
-                                       ResumeID = x.ResumeID,
-                                       CandidateID = x.CandidateID,
-                                       Title = x.Title,
-                                       Intro = x.Intro,
-                                       Autobiography = x.Autobiography,
-                                       WorkExperience = x.WorkExperience,
-                                       Certification = x.Certification,
-                                       Time = x.Time,
-                                       Address = x.Address,
-                                       Name = x.Name,
-                                       Sex = x.Sex,
-                                       Age = x.Age,
-                                       WishAddress = x.WishAddress,
-                                       Degree = x.Degree,
-                                       TagObj = x.skill
-                                   });
+                    b.skill.Any(z => z.TagId == resume.Skill)
+                ).Select(x => new ResumesOutput
+                    {
+                       ResumeID = x.ResumeID,
+                       CandidateID = x.CandidateID,
+                       Title = x.Title,
+                       Intro = x.Intro,
+                       Autobiography = x.Autobiography,
+                       WorkExperience = x.WorkExperience,
+                       Certification = x.Certification,
+                       Time = x.Time,
+                       Address = x.Address,
+                       Name = x.Name,
+                       Sex = x.Sex,
+                       Age = x.Age,
+                       WishAddress = x.WishAddress,
+                       Degree = x.Degree,
+                       TagObj = x.skill
+                    });
                 return temp;
             }
             else
             {
                 var temp = source.Select(c => new
-                            {
-                                ResumeID = c.ResumeId,
-                                CandidateID = c.CandidateId,
-                                Title = c.Title,
-                                Intro = c.Intro,
-                                Autobiography = c.Autobiography,
-                                WorkExperience = c.WorkExperience,
-                                Certification = c.Certification,
-                                WishAddress = c.Address,
-                                Time = c.Time,
-                                Name = c.Candidate.Name,
-                                Sex = c.Candidate.Sex,
-                                Birthday = c.Candidate.Birthday,
-                                Degree = c.Candidate.Degree,
-                                Address = c.Candidate.Address,
-                                skill = c.Tags.Select(z => new { z.TagId, z.TagName }),
-                                Age = c.Candidate.Birthday.HasValue ? CalculateAge(c.Candidate.Birthday.Value, today) : 0
-                            }).Where(b =>
-                                b.Age.ToString().Contains(resume.serchText) ||
-                                b.Address.Contains(resume.serchText) ||
-                                b.Address.Substring(4, 3) == resume.Area ||
-                                resume.serchText.Any(c => b.Name.IndexOf(c, StringComparison.OrdinalIgnoreCase) >= 0 ||  //不區分英文字母大小寫，逐一檢查
-                                b.skill.Any(z => z.TagId == resume.Skill)))
-                                    .Select(x => new ResumesOutput
-                                    {
-                                        ResumeID = x.ResumeID,
-                                        CandidateID = x.CandidateID,
-                                        Title = x.Title,
-                                        Intro = x.Intro,
-                                        Autobiography = x.Autobiography,
-                                        WorkExperience = x.WorkExperience,
-                                        Certification = x.Certification,
-                                        Time = x.Time,
-                                        Address = x.Address,
-                                        Name = x.Name,
-                                        Sex = x.Sex,
-                                        Age = x.Age,
-                                        WishAddress = x.WishAddress,
-                                        Degree = x.Degree,
-                                        TagObj = x.skill
-                                    }); 
+                {
+                    ResumeID = c.ResumeId,
+                    CandidateID = c.CandidateId,
+                    Title = c.Title,
+                    Intro = c.Intro,
+                    Autobiography = c.Autobiography,
+                    WorkExperience = c.WorkExperience,
+                    Certification = c.Certification,
+                    WishAddress = c.Address,
+                    Time = c.Time,
+                    Name = c.Candidate.Name,
+                    Sex = c.Candidate.Sex,
+                    Birthday = c.Candidate.Birthday,
+                    Degree = c.Candidate.Degree,
+                    Address = c.Candidate.Address,
+                    skill = c.Tags.Select(z => new { z.TagId, z.TagName }),
+                    Age = c.Candidate.Birthday.HasValue ? CalculateAge(c.Candidate.Birthday.Value, today) : 0
+                }).Where(b =>
+                    b.Sex == resume.Sex ||
+                    b.Age.ToString().Contains(resume.serchText) ||
+                    b.Address.Contains(resume.serchText.Replace("臺", "台")) ||
+                    b.Address.Substring(4, 3) == resume.Area ||
+                    resume.serchText.Any(c => b.Name.IndexOf(c, StringComparison.OrdinalIgnoreCase) >= 0)||  //不區分英文字母大小寫，逐一檢查
+                    b.skill.Any(z => z.TagId == resume.Skill)
+                ).Select(x => new ResumesOutput
+                 {
+                     ResumeID = x.ResumeID,
+                     CandidateID = x.CandidateID,
+                     Title = x.Title,
+                     Intro = x.Intro,
+                     Autobiography = x.Autobiography,
+                     WorkExperience = x.WorkExperience,
+                     Certification = x.Certification,
+                     Time = x.Time,
+                     Address = x.Address,
+                     Name = x.Name, 
+                     Sex = x.Sex,
+                     Age = x.Age,
+                     WishAddress = x.WishAddress,
+                     Degree = x.Degree,
+                     TagObj = x.skill
+                 }); 
                   return temp;       
             }
            
@@ -204,11 +206,9 @@ namespace JobHunting.Controllers
         {
             return address.Replace("臺", "台");
         }
-        public void AddResume(Resume resume)
+        public void EditResume(ResumeInputModel resume)
         {
-            resume.Address = NormalizeAddress(resume.Address);
-            _context.Resumes.Add(resume);
-            _context.SaveChanges();
+            resume.Area = NormalizeAddress(resume.Area);
         }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
