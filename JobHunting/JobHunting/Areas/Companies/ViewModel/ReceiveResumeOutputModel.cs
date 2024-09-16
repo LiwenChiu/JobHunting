962ea7999bf1473c7e2c8df9ab39e417bed7609a
willy <weigl0731@gmail.com>
2024-09-16 21:06:58 +0800

    add 投遞履歷畫面與暫存按鈕

--- JobHunting/JobHunting/Areas/Companies/ViewModel/ReceiveResumeOutputModel.cs
+++ JobHunting/JobHunting/Areas/Companies/ViewModel/ReceiveResumeOutputModel.cs
@@ -1,7 +1,39 @@
-﻿namespace JobHunting.Areas.Companies.ViewModel
+﻿using JobHunting.Areas.Companies.Models;
+using JobHunting.Areas.Companies.ViewModel;
+using JobHunting.Models;
+using System.ComponentModel;
+
+namespace JobHunting.Areas.Companies.ViewModel
 {
-    public class ResumeStorageViewModel
+    public class ReceiveResumeOutputModel
     {
+        [DisplayName("應徵日期")]
+        public DateOnly? ApplyDate { get; set; }
+        [DisplayName("職缺名稱")]
+        public string Title { get; set; }
+        [DisplayName("職缺類別")]
+        public string TitleClassName { get; set; }
+
+
+        public List<int> TitleCategoryId { get; set; }
+        public int ResumeOpeningRecordId { get; set; }
+
+        public int? ResumeId { get; set; }
+
+        public int? OpeningId { get; set; }
+
+        public string OpeningTitle { get; set; }
+
+        public int? CompanyId { get; set; }
+
+        public string CompanyName { get; set; }
+
+
+        public bool InterviewYN { get; set; }
+
+        public bool HireYN { get; set; }
+
+
         public string Email { get; set; }
 
         public string Password { get; set; }
@@ -15,19 +47,16 @@
         public string Phone { get; set; }
 
         public string Address { get; set; }
-        public int CompanyId { get; set; }
+
 
         public string Degree { get; set; }
         public string? Time { get; set; }
-        public string Title { get; set; }
-        public string OpeningTitle { get; set; }
+
         public byte[] Headshot { get; set; }
         public byte[] Certification { get; set; }
         public string? WorkExperience { get; set; }
         public string? Autobiography { get; set; }
         public int CandidateId { get; set; }
-        public int ResumeId { get; set; }
-        public int OpeningId { get; set; }
         public bool ReleaseYN { get; set; }
         public string? Intro { get; set; }
         public List<int> TagId { get; set; }
@@ -35,7 +64,5 @@
 
         public List<IFormFile>? CertificationImageFile { get; set; }
         public IFormFile? HeadshotImageFile { get; set; }
-        
-
     }
 }
