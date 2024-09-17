e0bc16d3f254d9ca22395b4f14f72dabddea9c03
Daniel <dancraft3746@gmail.com>
2024-09-17 23:14:17 +0800

    Companies & Candidates 的 Notifications

---
+++ JobHunting/JobHunting/Areas/Candidates/ViewModels/NotificationCandidateFullOutputViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace JobHunting.Areas.Candidates.ViewModels
+{
+    public class NotificationCandidateFullOutputViewModel
+    {
+        public IEnumerable<NotificationCandidateLessOutputViewModel> candidateNotificationsFilter { get; set; } = new List<NotificationCandidateLessOutputViewModel>();
+
+        public int totalDataCount { get; set; }
+    }
+}
---
+++ JobHunting/JobHunting/Areas/Companies/ViewModel/NotificationCompanyDeleteNotificationInputViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace JobHunting.Areas.Companies.ViewModel
+{
+    public class NotificationCompanyDeleteNotificationInputViewModel
+    {
+        public int CompanyId { get; set; }
+
+        public int NotificationId { get; set; }
+    }
+}
