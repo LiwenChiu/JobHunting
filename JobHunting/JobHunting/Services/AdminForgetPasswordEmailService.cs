b7dc8fb7801ec2bee18610b57b85f48a5cc78b52
Chiu <liwen2391062@gmail.com>
2024-09-28 13:03:32 +0800

    no message

---
+++ JobHunting/JobHunting/Services/AdminForgetPasswordEmailService.cs
@@ -0,0 +1,68 @@
+﻿using System.Composition;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+
+namespace JobHunting.Services
+{
+    public class AdminForgetPasswordEmailService
+    {
+        public string GenerateVerificationToken(string email)
+        {
+            //token = guid唯一碼
+            string token = Guid.NewGuid().ToString();
+
+            //設定驗證信為5分鐘到期
+            DateTime expiry = DateTime.UtcNow.AddMinutes(10);
+
+            string encodedEmail = Convert.ToBase64String(Encoding.UTF8.GetBytes(email));
+            string encodedToken = Convert.ToBase64String(Encoding.UTF8.GetBytes(token));
+
+            string verificationLink = $"https://localhost:7169/ForgetPassword/CompanyVerifyEmail?token={encodedToken}&email={encodedEmail}&expiry={expiry.Ticks}";
+            //string verificationLink = $"https://duckjobhunting.azurewebsites.net/CompanyVerifyEmail?token={encodedToken}&email={encodedEmail}&expiry={expiry.Ticks}";
+
+            return verificationLink;
+        }
+
+        public void SendMail(string receiveMail, string subject)
+        {
+            //驗證連結，會調用HomeController的VerifyEmail方法來判斷並修改驗證狀態
+            string verifyUrl = GenerateVerificationToken(receiveMail);
+
+            // 建立郵件內容
+            string emailBody = $@"
+                <html>
+                <body>
+                   <h1>忘記密碼通知</h1>
+                    <p>你好，若要重新設定密碼，請點擊下方連結</p>
+                    <p><a href='{verifyUrl}'>點擊這裡重設密碼</a></p>
+                    <p>如果您無法點擊連結，請將以下網址複製到瀏覽器中打開：</p>
+                    <p>{verifyUrl}</p>
+                    <br/>
+                    <p>此為系統自動發送的郵件，請勿回覆。</p>
+                </body>
+                </html>";
+
+            // 使用 Google Mail Server 發信
+            string GoogleID = "TIM102FirstGroup@gmail.com"; //Google 發信帳號
+            string TempPwd = "mgrc fdks oypm sewa"; //應用程式密碼
+            //string ReceiveMail = "TIM102FirstGroup@gmail.com"; //接收信箱
+
+            string SmtpServer = "smtp.gmail.com";
+            int SmtpPort = 587;
+            MailMessage mms = new MailMessage();
+            mms.From = new MailAddress(GoogleID);
+            mms.Subject = subject; //信件主題
+            mms.Body = emailBody; //信件內容
+            mms.IsBodyHtml = true;
+            mms.SubjectEncoding = Encoding.UTF8;
+            mms.To.Add(new MailAddress(receiveMail));
+            using (SmtpClient client = new SmtpClient(SmtpServer, SmtpPort))
+            {
+                client.EnableSsl = true;
+                client.Credentials = new NetworkCredential(GoogleID, TempPwd);//寄信帳密 
+                client.Send(mms); //寄出信件
+            }
+        }
+    }
+}
---
+++ JobHunting/JobHunting/Services/NewebPaySearchNonCurrentService.cs
@@ -0,0 +1,276 @@
+﻿using JobHunting.Models;
+using JobHunting.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace JobHunting.Services
+{
+    public class NewebPaySearchNonCurrentService
+    {
+        private readonly DuckContext _context;
+
+        public NewebPaySearchNonCurrentService(DuckContext context)
+        {
+            _context = context;
+        }
+
+        public async Task NewebPaySearchStatusFalse()
+        {
+            var taiwanTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Taipei Standard Time");
+            var nowTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, taiwanTimeZone);
+
+            var companyOrders = _context.CompanyOrders.Where(co => co.ExpireDate.HasValue && !co.PayDate.HasValue).Where(co => DateTime.Compare((DateTime)co.ExpireDate, nowTime) < 0);
+
+            if (!companyOrders.IsNullOrEmpty())
+            {
+                foreach (var companyOrder in companyOrders)
+                {
+                    await SendToNewebPaySearchNonCurrentRecurring(companyOrder);
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// 傳送查詢要求至藍新金流
+        /// </summary>
+        /// <param name="inModel"></param>
+        /// <returns></returns>
+        ///POST: Companies/CompanyOrders/SendToNewebPaySearch
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public async Task SendToNewebPaySearchNonCurrentRecurring(CompanyOrder companyOrder)
+        {
+            SendToNewebPaySearchRecurringOutViewModel outModel = new SendToNewebPaySearchRecurringOutViewModel();
+
+            IConfiguration Config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+            var MerchantID = Config.GetSection("MerchantID").Value;
+            int Price = decimal.ToInt32(companyOrder.Price);
+
+            List<KeyValuePair<string, string>> CheckValue = new List<KeyValuePair<string, string>>();
+            CheckValue.Add(new KeyValuePair<string, string>("Amt", Price.ToString()));
+            CheckValue.Add(new KeyValuePair<string, string>("MerchantID", MerchantID));
+            CheckValue.Add(new KeyValuePair<string, string>("MerchantOrderNo", companyOrder.OrderId));
+
+            string CheckValueParam = string.Join("&", CheckValue.Select(x => $"{x.Key}={x.Value}"));
+
+            outModel.MerchantID = MerchantID;
+            outModel.Version = "1.3";
+            outModel.RespondType = "JSON";
+
+            string HashIV = Config.GetSection("HashIV").Value;//API 串接密碼
+            string HashIVStr = $"IV={HashIV}";
+
+            string HashKey = Config.GetSection("HashKey").Value;//API 串接金鑰
+            string HashKeyStr = $"Key={HashKey}";
+
+            string[] CheckValueList = [HashIVStr, CheckValueParam, HashKeyStr];
+
+            string CheckValueStr = string.Join("&", CheckValueList);
+
+            string CheckValueEncrypt = EncryptSHA256(CheckValueStr);
+
+            outModel.CheckValue = CheckValueEncrypt;
+            outModel.TimeStamp = DateTimeOffset.Now.ToOffset(new TimeSpan(8, 0, 0)).ToUnixTimeSeconds().ToString();
+            outModel.MerchantOrderNo = companyOrder.OrderId;
+            outModel.Amt = Price;
+
+            var outModelReturn = SearchPostFormDataAsync(outModel).Result;
+            var outModelReturnResult = outModelReturn.Result;
+            if (outModelReturnResult == null)
+            {
+                return;
+            }
+            string TradeStatus = outModelReturnResult.TradeStatus;
+
+            if (outModelReturnResult.MerchantID != MerchantID || outModelReturnResult.MerchantOrderNo != companyOrder.OrderId)
+            {
+                return;
+            }
+
+            var StatusType = TradeStatus switch
+            {
+                string type when type == "0" => "尚未付款",
+                string type when type == "1" => "付款成功",
+                string type when type == "2" => "付款失敗",
+                string type when type == "3" => "已取消",
+                string type when type == "6" => "退款",
+                _ => "錯誤",
+            };
+
+            if (TradeStatus != "1")
+            {
+                companyOrder.StatusType = StatusType;
+                companyOrder.ExpireDate = null;
+
+                _context.Entry(companyOrder).State = EntityState.Modified;
+
+                try
+                {
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    return;
+                }
+
+                return;
+            }
+
+            if (TradeStatus == "1")
+            {
+                companyOrder.ExpireDate = null;
+                companyOrder.StatusType = StatusType;
+                companyOrder.NewebPayStatus = outModelReturn.Status;
+                companyOrder.NewebPayMessage = outModelReturn.Message;
+
+                _context.Entry(companyOrder).State = EntityState.Modified;
+
+                try
+                {
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    return;
+                }
+            }
+
+            return;
+        }
+
+        // Search訂單 發送 form-data 的 POST 請求
+        private static async Task<TradeInfoResponse> SearchPostFormDataAsync(SendToNewebPaySearchRecurringOutViewModel outModel)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                // 設置 form-data
+                var formData = new Dictionary<string, string>
+            {
+                { "MerchantID", outModel.MerchantID },
+                { "Version", outModel.Version },
+                { "RespondType", outModel.RespondType },
+                { "CheckValue", outModel.CheckValue },
+                { "TimeStamp", outModel.TimeStamp },
+                { "MerchantOrderNo", outModel.MerchantOrderNo },
+                { "Amt", outModel.Amt.ToString() },
+            };
+
+                var content = new FormUrlEncodedContent(formData);
+
+                // 發送 POST 請求並接收回應
+                HttpResponseMessage response = await client.PostAsync("https://ccore.newebpay.com/API/QueryTradeInfo", content);
+                //HttpResponseMessage response = await client.PostAsync("https://core.newebpay.com/API/QueryTradeInfo", content);
+                response.EnsureSuccessStatusCode();
+
+                // 讀取回應內容為字符串
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                // 將回應的 JSON 反序列化為 TradeInfoResponse 類型
+                TradeInfoResponse tradeInfoResponse = JsonConvert.DeserializeObject<TradeInfoResponse>(responseBody);
+
+                return tradeInfoResponse;
+            }
+        }
+
+        public class TradeInfoResponse
+        {
+            public string Status { get; set; }
+            public string Message { get; set; }
+            public Result Result { get; set; }
+        }
+
+        public class Result
+        {
+            public string MerchantID { get; set; }
+
+            public int Amt { get; set; }
+
+            public string TradeNo { get; set; }
+
+            public string MerchantOrderNo { get; set; }
+
+            public string TradeStatus { get; set; }
+
+            public string PaymentType { get; set; }
+
+            public DateTime? PayTime { get; set; }
+        }
+
+        /// <summary>
+        /// 加密後再轉 16 進制字串
+        /// </summary>
+        /// <param name="source">加密前字串</param>
+        /// <param name="cryptoKey">加密金鑰</param>
+        /// <param name="cryptoIV">cryptoIV</param>
+        /// <returns>加密後的字串</returns>
+        public string EncryptAESHex(string source, string cryptoKey, string cryptoIV)
+        {
+            string result = string.Empty;
+
+            if (!string.IsNullOrEmpty(source))
+            {
+                var encryptValue = EncryptAES(Encoding.UTF8.GetBytes(source), cryptoKey, cryptoIV);
+
+                if (encryptValue != null)
+                {
+                    result = BitConverter.ToString(encryptValue)?.Replace("-", string.Empty)?.ToLower();
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 字串加密AES
+        /// </summary>
+        /// <param name="source">加密前字串</param>
+        /// <param name="cryptoKey">加密金鑰</param>
+        /// <param name="cryptoIV">cryptoIV</param>
+        /// <returns>加密後字串</returns>
+        public byte[] EncryptAES(byte[] source, string cryptoKey, string cryptoIV)
+        {
+            byte[] dataKey = Encoding.UTF8.GetBytes(cryptoKey);
+            byte[] dataIV = Encoding.UTF8.GetBytes(cryptoIV);
+
+            using (var aes = System.Security.Cryptography.Aes.Create())
+            {
+                aes.Mode = System.Security.Cryptography.CipherMode.CBC;
+                aes.Padding = System.Security.Cryptography.PaddingMode.PKCS7;
+                aes.Key = dataKey;
+                aes.IV = dataIV;
+
+                using (var encryptor = aes.CreateEncryptor())
+                {
+                    return encryptor.TransformFinalBlock(source, 0, source.Length);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 字串加密SHA256
+        /// </summary>
+        /// <param name="source">加密前字串</param>
+        /// <returns>加密後字串</returns>
+        public string EncryptSHA256(string source)
+        {
+            string result = string.Empty;
+
+            using (System.Security.Cryptography.SHA256 algorithm = System.Security.Cryptography.SHA256.Create())
+            {
+                var hash = algorithm.ComputeHash(Encoding.UTF8.GetBytes(source));
+
+                if (hash != null)
+                {
+                    result = BitConverter.ToString(hash)?.Replace("-", string.Empty)?.ToUpper();
+                }
+
+            }
+            return result;
+        }
+    }
+}
