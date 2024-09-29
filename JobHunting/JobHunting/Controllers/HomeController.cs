91d75656b0e22b4624588191a304db6998b5a8e4
Tony <godjames226@gmail.com>
2024-09-29 16:08:51 +0800

    update 履歷跟職缺首頁

--- JobHunting/JobHunting/Controllers/HomeController.cs
+++ JobHunting/JobHunting/Controllers/HomeController.cs
@@ -357,14 +357,14 @@ namespace JobHunting.Controllers
         }
 
 
-        public IActionResult CompanyClassSelect()
+        public IActionResult TitleClassSelect()
         {
-            var source = _context.CompanyCategories.Include(a => a.CompanyClasses);
-            var temp = source.Select(b => new CompanyClassSelectViewModelcs
+            var source = _context.TitleCategories.Include(a => a.TitleClasses);
+            var temp = source.Select(b => new TitleClassSelectViewModelcs
             {
-                CompanyClassObj = b.CompanyClasses.Select(x => new { x.CompanyClassId, x.CompanyClassName }),
-                CompanyCategoryId = b.CompanyCategoryId,
-                CompanyCategoryName = b.CompanyCategoryName,
+                TitleClassObj = b.TitleClasses.Select(x => new { x.TitleClassId, x.TitleClassName }),
+                TitleCategoryId = b.TitleCategoryId,
+                TitleCategoryName = b.TitleCategoryName,
             });
             return Json(temp);
         }
@@ -885,7 +885,7 @@ namespace JobHunting.Controllers
             EditResume(opening);
             if (candidateIdClaim == null)
             {
-                var sourceUnlogin = _context.Openings.AsNoTracking().Include(a => a.Company).Include(a => a.Candidates).Include(x => x.Tags).Where(y => y.ReleaseYN == true)
+                var sourceUnlogin = _context.Openings.AsNoTracking().Include(a => a.Company).Include(a => a.Candidates).Include(x => x.Tags).Include(y => y.TitleClasses).Where(y => y.ReleaseYN == true)
                         .Select(c => new
                         {
                             OpeningId = c.OpeningId,
@@ -902,7 +902,7 @@ namespace JobHunting.Controllers
                             ContactName = c.ContactName,
                             ContactPhone = c.ContactPhone,
                             CompanyName = c.Company.CompanyName,
-                            ClassNumber = c.Company.CompanyClassId,
+                            TitleClass = c.TitleClasses.Select(x => new { x.TitleClassId, x.TitleClassName }),
                             LikeYN = false,
                         });
                 if (!opening.SearchText.IsNullOrEmpty())
@@ -920,10 +920,10 @@ namespace JobHunting.Controllers
                     sourceUnlogin = sourceUnlogin.Where(b =>
                              b.Address.Contains(opening.AreaName));
                 }
-                if (!opening.ClassNumber.IsNullOrEmpty())
+                if (opening.ClassNumber != null)
                 {
                     sourceUnlogin = sourceUnlogin.Where(b =>
-                             b.ClassNumber == opening.ClassNumber);
+                             b.TitleClass.Any(x => x.TitleClassId == opening.ClassNumber));
                 }
                 if (opening.Salary != null)
                 {
@@ -931,7 +931,7 @@ namespace JobHunting.Controllers
                              opening.Salary <= b.SalaryMin ||
                              b.SalaryMax >= opening.Salary );
                 }
-                if (opening.SearchText == "" && opening.AreaName == "" && opening.ClassNumber == "" && opening.Salary == null)
+                if (opening.SearchText == "" && opening.AreaName == "" && opening.ClassNumber == null && opening.Salary == null)
                 {
                     sourceUnlogin = sourceUnlogin;
                 }
@@ -951,6 +951,7 @@ namespace JobHunting.Controllers
                     ContactName = c.ContactName,
                     ContactPhone = c.ContactPhone,
                     CompanyName = c.CompanyName,
+                    TitleClass = c.TitleClass,
                     LikeYN = null,
                 });
                 var openingSelectOutput = new OpeningSelectOutputViewModel();
@@ -979,7 +980,7 @@ namespace JobHunting.Controllers
                             ContactName = c.ContactName,
                             ContactPhone = c.ContactPhone,
                             CompanyName = c.Company.CompanyName,
-                            ClassNumber = c.Company.CompanyClassId,
+                            TitleClass = c.TitleClasses.Select(x => new { x.TitleClassId, x.TitleClassName }),
                             LikeYN = c.Candidates.Where(c => c.CandidateId == candidateId).FirstOrDefault() != null,
                         });
                 if (!opening.SearchText.IsNullOrEmpty())
@@ -997,10 +998,10 @@ namespace JobHunting.Controllers
                     sourcelogin = sourcelogin.Where(b =>
                              b.Address.Contains(opening.AreaName));
                 }
-                if (!opening.ClassNumber.IsNullOrEmpty())
+                if (opening.ClassNumber != null)
                 {
                     sourcelogin = sourcelogin.Where(b =>
-                             b.ClassNumber == opening.ClassNumber);
+                             b.TitleClass.Any(x => x.TitleClassId == opening.ClassNumber));
                 }
                 if (opening.Salary != null)
                 {
@@ -1008,7 +1009,7 @@ namespace JobHunting.Controllers
                              opening.Salary <= b.SalaryMin ||
                              b.SalaryMax >= opening.Salary);
                 }
-                if (opening.SearchText == "" && opening.AreaName == "" && opening.ClassNumber == "" && opening.Salary == null)
+                if (opening.SearchText == "" && opening.AreaName == "" && opening.ClassNumber == null && opening.Salary == null)
                 {
                     sourcelogin = sourcelogin;
                 }
@@ -1028,6 +1029,7 @@ namespace JobHunting.Controllers
                     ContactName = c.ContactName,
                     ContactPhone = c.ContactPhone,
                     CompanyName = c.CompanyName,
+                    TitleClass = c.TitleClass,
                     LikeYN = c.LikeYN,
                 });
                 var openingSelectOutput = new OpeningSelectOutputViewModel();
---
+++ JobHunting/JobHunting/ViewModel/TitleClassSelectViewModelcs.cs
@@ -0,0 +1,9 @@
+﻿namespace JobHunting.ViewModel
+{
+    public class TitleClassSelectViewModelcs
+    {
+        public object TitleClassObj { get; set; }
+        public int TitleCategoryId { get; set; }
+        public string TitleCategoryName { get; set; }
+    }
+}
